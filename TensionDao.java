cdaba467183c06b184d0542509f930ddca901462
Stix-FR <contact@huggybear.fr>
2025-02-21 16:19:16 +0100

    Create TensionDao.java

---
+++ TensionDao.java
@@ -0,0 +1,15 @@
+package com.example.healthtracker;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import java.util.List;
+
+@Dao
+public interface TensionDao {
+    @Insert
+    void insert(TensionEntity tension);
+
+    @Query("SELECT * FROM tension")
+    List<TensionEntity> getAll();
+}
