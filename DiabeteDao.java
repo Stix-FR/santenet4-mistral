5eb338ccbf1e6dd410588bced59b26713beae4e5
Stix-FR <contact@huggybear.fr>
2025-02-21 16:19:59 +0100

    Create DiabeteDao.java

---
+++ DiabeteDao.java
@@ -0,0 +1,15 @@
+package com.example.healthtracker;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import java.util.List;
+
+@Dao
+public interface DiabeteDao {
+    @Insert
+    void insert(DiabeteEntity diabete);
+
+    @Query("SELECT * FROM diabete")
+    List<DiabeteEntity> getAll();
+}
